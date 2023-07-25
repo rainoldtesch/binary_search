9c1db24d0727e76842f193038299015fd2285092
Rainold Tesch <rainold.tesch@gmail.com>
2023-07-25 20:53:31 -0300

    Colocando alteração de retorno

--- src/FileReader.java
+++ src/FileReader.java
@@ -10,28 +10,26 @@ public class FileReader {
             // Leitura do arquivo usando Java NIO
             String conteudo = new String(Files.readAllBytes(Paths.get(nomeArquivo)));
 
-            // Quebrar a linha em um array de strings usando a vírgula como separador
+            // Quebrar a linha em um array de strings usando a vírgula como separador:
             String[] numerosString = conteudo.split(", ");
 
-            // Criar um array de inteiros para armazenar os números
+            // Criar um array de inteiros para armazenar os números:
             int[] arrayDeInteiros = new int[numerosString.length];
 
-            // Converter cada string para um número inteiro e armazená-lo no array
+            // Converter cada string para um número inteiro e armazená-lo no array:
             for (int i = 0; i < numerosString.length; i++) {
                 arrayDeInteiros[i] = Integer.parseInt(numerosString[i].trim());
             }
 
-            // Exemplo: Imprime os elementos do array
+            // Exemplo: imprime os elementos do array
             for (int i = 0; i < arrayDeInteiros.length; i++) {
                 System.out.println("Elemento " + i + ": " + arrayDeInteiros[i]);
             }
-
             return arrayDeInteiros;
 
         } catch (IOException e) {
             System.out.println("Erro ao ler o arquivo: " + e.getMessage());
         }
-
-        return null;
+        return new int[]{0};
     }
 }
